827c02b0232fea65c35df36ca39d3be6ce8e2907
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-04-24 07:46:22 -0300

    Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ class Personagem {
         super(x, y, largura, altura);
         this.#vida = 0;
         this.#força = 0;
-        this.mana = 0
+        this.#mana = 0
+    }
+}
+class Guerreiro extends Personagem {
+    constructor(x, y, largura, altura) {
+        super(x, y, largura, altura);
+    }
+}
+class Mago extends Personagem {
+    constructor(x, y, largura, altura) {
+        super(x, y, largura, altura);
+    }
+}
+class Arqueiro extends Personagem {
+    constructor(x, y, largura, altura) {
+        super(x, y, largura, altura);
     }
 }
