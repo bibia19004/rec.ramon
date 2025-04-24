93d469f00770649e79d1e00ce6b5adb647fcb64c
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-04-24 08:27:38 -0300

    Encapsulamento de propriedades sensíveis com getters e setters

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ class Personagem {
     #mana;
     #esquivar;
     #chute;
-    constructor(x, y, largura, altura) {
-        super(x, y, largura, altura);
+    constructor(vida, força, mana) {
+        super(vida, força, mana);
         this.#vida = 0;
         this.#força = 0;
         this.#mana = 0
@@ -15,25 +15,51 @@ class Personagem {
     atacar(){
          this.#chute = true;
     }
+    get vida() {
+        return this.#vida;
+    }
+    get força() {
+        return this.#força;
+    }
+    get mana() {
+        return this.#mana;
+    }
 }
 class Guerreiro extends Personagem {
     #espada;
     #escudo;
-    constructor(x, y, largura, altura) {
-        super(x, y, largura, altura);
+    constructor(vida, força, mana) {
+        super(vida, força, mana);
+        this.#espada = false;
+        this.#escudo = false;
     }
     defender() {
         this.#escudo = true; }
     atacar(){
         this.#espada = true;
     }
+    get espada() {
+        return this.#espada;
+    }
+    set espada(valor) {
+        this.#espada = valor;
+    }
+    get escudo() {
+        return this.#escudo;
+    }
+    set escudo(valor) {
+        this.#escudo = valor;
+    }
 }
 class Mago extends Personagem {
     #magia;
     #feitiço;
     #maldição;
-    constructor(x, y, largura, altura) {
-        super(x, y, largura, altura);
+    constructor(vida, força, mana) {
+        super(vida, força, mana);
+        this.#magia = false;
+        this.#feitiço = false;
+        this.#maldição = false;
     }
     UsarMagia(){
         this.#magia = true;
@@ -43,17 +69,49 @@ class Mago extends Personagem {
     defender(){
         this.#maldição = true;
     }
+    get magia() {
+        return this.#magia;
+    }
+    set magia(valor) {
+        this.#magia = valor;
+    }
+    get feitiço() {
+        return this.#feitiço;
+    }
+    set feitiço(valor) {
+        this.#feitiço = valor;
+    }
+    get maldição() {
+        return this.#maldição;
+    }
+    set maldição(valor) {
+        this.#maldição = valor;
+    }
 }
 class Arqueiro extends Personagem {
     #atirarFlecha;
     #bloqueio;
-    constructor(x, y, largura, altura) {
-        super(x, y, largura, altura);
+    constructor(vida, força, mana) {
+        super(vida, força, mana);
+        this.#atirarFlecha = false;
+        this.#bloqueio = false;
     }
     atacar(){
         this.#atirarFlecha = true;
     }
     defender(){
         this.#bloqueio = true;
+    }
+    get atirarFlecha() {
+        return this.#atirarFlecha;
+    }
+    set atirarFlecha(valor) {
+        this.#atirarFlecha = valor;
+    }
+    get bloqueio() {
+        return this.#bloqueio;
+    }
+    set bloqueio(valor) {
+        this.#bloqueio = valor;
     }
 }
