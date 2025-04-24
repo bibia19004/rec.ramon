374a1506acece57d758401b78be0d01a31e7d855
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-04-24 08:48:53 -0300

    adicionado funcionalidades de reduzir a vida do oponente, gerir o jogo, combate e turnos

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ class Personagem {
     }
     defender() {
         this.#esquivar = true; }
-    atacar(){
+    atacar(oponente){
          this.#chute = true;
+         if (oponente && typeof oponente.vida === 'number') {
+            oponente.vida -= this.#força;
+            if (oponente.vida < 0) {
+                oponente.vida = 0;
+            }
+        }
     }
     get vida() {
         return this.#vida;
@@ -28,15 +34,22 @@ class Personagem {
 class Guerreiro extends Personagem {
     #espada;
     #escudo;
+    #força;
     constructor(vida, força, mana) {
         super(vida, força, mana);
         this.#espada = false;
         this.#escudo = false;
     }
-    defender() {
+    defender(oponente) {
         this.#escudo = true; }
     atacar(){
         this.#espada = true;
+        if (oponente && typeof oponente.vida === 'number') {
+            oponente.vida -= this.#força * 1.2;
+            if (oponente.vida < 0) {
+                oponente.vida = 0;
+            }
+        }
     }
     get espada() {
         return this.#espada;
@@ -55,6 +68,7 @@ class Mago extends Personagem {
     #magia;
     #feitiço;
     #maldição;
+    #força;
     constructor(vida, força, mana) {
         super(vida, força, mana);
         this.#magia = false;
@@ -64,8 +78,15 @@ class Mago extends Personagem {
     UsarMagia(){
         this.#magia = true;
     }
-    atacar(){
-        this.#feitiço = true;}
+    atacar(oponente){
+        this.#feitiço = true;
+        if (oponente && typeof oponente.vida === 'number') {
+            oponente.vida -= this.#força * 1.5;
+            if (oponente.vida < 0) {
+                oponente.vida = 0;
+            }
+        }
+    }
     defender(){
         this.#maldição = true;
     }
@@ -91,13 +112,20 @@ class Mago extends Personagem {
 class Arqueiro extends Personagem {
     #atirarFlecha;
     #bloqueio;
+    #força;
     constructor(vida, força, mana) {
         super(vida, força, mana);
         this.#atirarFlecha = false;
         this.#bloqueio = false;
     }
-    atacar(){
+    atacar(oponente){
         this.#atirarFlecha = true;
+        if (oponente && typeof oponente.vida === 'number') {
+            oponente.vida -= this.#força * 1.1; 
+            if (oponente.vida < 0) {
+                oponente.vida = 0;
+            }
+        }
     }
     defender(){
         this.#bloqueio = true;
@@ -115,3 +143,102 @@ class Arqueiro extends Personagem {
         this.#bloqueio = valor;
     }
 }
+const personagens = [];
+
+function executarAtaques(oponente) {
+    personagens.forEach(personagem => {
+        personagem.atacar(oponente);
+        console.log(`${personagem.constructor.name} atacou! Vida do oponente: ${oponente.vida}`);
+    });
+}
+
+const guerreiro = new Guerreiro(100, 20, 10);
+const mago = new Mago(80, 15, 50);
+const arqueiro = new Arqueiro(90, 18, 30);
+
+personagens.push(guerreiro, mago, arqueiro);
+
+const oponente = { vida: 200 };
+
+executarAtaques(oponente);
+
+function batalhar(p1, p2) {
+    console.log(`A batalha começou entre ${p1.constructor.name} e ${p2.constructor.name}!`);
+    
+    while (p1.vida > 0 && p2.vida > 0) {
+        p1.atacar(p2);
+        console.log(`${p1.constructor.name} atacou! Vida de ${p2.constructor.name}: ${p2.vida}`);
+        
+        if (p2.vida <= 0) {
+            console.log(`${p2.constructor.name} foi derrotado!`);
+            break;
+        }
+        
+        p2.atacar(p1);
+        console.log(`${p2.constructor.name} atacou! Vida de ${p1.constructor.name}: ${p1.vida}`);
+        
+        if (p1.vida <= 0) {
+            console.log(`${p1.constructor.name} foi derrotado!`);
+            break;
+        }
+    }
+}
+
+const Guerreiro = new Guerreiro(100, 20, 10);
+const Mago = new Mago(80, 15, 50);
+
+batalhar(guerreiro, mago);
+
+function turno(p1, p2) {
+    console.log(`Turno iniciado entre ${p1.constructor.name} e ${p2.constructor.name}!`);
+
+    if (p1 instanceof Guerreiro) {
+        p1.atacar(p2);
+        console.log(`${p1.constructor.name} atacou com sua espada! Vida de ${p2.constructor.name}: ${p2.vida}`);
+    } else if (p1 instanceof Mago) {
+        p1.UsarMagia();
+        console.log(`${p1.constructor.name} usou magia!`);
+        p1.atacar(p2);
+        console.log(`${p1.constructor.name} atacou com feitiço! Vida de ${p2.constructor.name}: ${p2.vida}`);
+    } else if (p1 instanceof Arqueiro) {
+        p1.atacar(p2);
+        console.log(`${p1.constructor.name} atirou uma flecha! Vida de ${p2.constructor.name}: ${p2.vida}`);
+    } else {
+        p1.atacar(p2);
+        console.log(`${p1.constructor.name} atacou! Vida de ${p2.constructor.name}: ${p2.vida}`);
+    }
+
+    if (p2.vida <= 0) {
+        console.log(`${p2.constructor.name} foi derrotado!`);
+        return;
+    }
+
+    if (p2 instanceof Guerreiro) {
+        p2.defender();
+        console.log(`${p2.constructor.name} levantou o escudo para se defender!`);
+        p2.atacar(p1);
+        console.log(`${p2.constructor.name} atacou com sua espada! Vida de ${p1.constructor.name}: ${p1.vida}`);
+    } else if (p2 instanceof Mago) {
+        p2.defender();
+        console.log(`${p2.constructor.name} lançou uma maldição para se defender!`);
+        p2.atacar(p1);
+        console.log(`${p2.constructor.name} atacou com feitiço! Vida de ${p1.constructor.name}: ${p1.vida}`);
+    } else if (p2 instanceof Arqueiro) {
+        p2.defender();
+        console.log(`${p2.constructor.name} se esquivou!`);
+        p2.atacar(p1);
+        console.log(`${p2.constructor.name} atirou uma flecha! Vida de ${p1.constructor.name}: ${p1.vida}`);
+    } else {
+        p2.atacar(p1);
+        console.log(`${p2.constructor.name} atacou! Vida de ${p1.constructor.name}: ${p1.vida}`);
+    }
+
+    if (p1.vida <= 0) {
+        console.log(`${p1.constructor.name} foi derrotado!`);
+    }
+}
+
+const Guerreiro = new Guerreiro(100, 20, 10);
+const Mago = new Mago(80, 15, 50);
+
+turno(guerreiro, mago);
