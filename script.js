7fa7f4ac84c7d3da19f819a8f21af8df7146316f
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-04-24 08:09:13 -0300

    Aplicação de polimorfismo no método atacar

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ class Personagem {
     #vida;
     #força;
     #mana;
-    #escudo;
+    #esquivar;
+    #chute;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
         this.#vida = 0;
@@ -10,33 +11,49 @@ class Personagem {
         this.#mana = 0
     }
     defender() {
-        this.#escudo = true; 
+        this.#esquivar = true; }
+    atacar(){
+         this.#chute = true;
     }
 }
 class Guerreiro extends Personagem {
     #espada;
+    #escudo;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
     }
+    defender() {
+        this.#escudo = true; }
     atacar(){
         this.#espada = true;
     }
 }
 class Mago extends Personagem {
     #magia;
+    #feitiço;
+    #maldição;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
     }
     UsarMagia(){
         this.#magia = true;
     }
+    atacar(){
+        this.#feitiço = true;}
+    defender(){
+        this.#maldição = true;
+    }
 }
 class Arqueiro extends Personagem {
-    #flecha;
+    #atirarFlecha;
+    #bloqueio;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
     }
-    AtirarFlecha(){
-        this.#flecha = true;
+    atacar(){
+        this.#atirarFlecha = true;
+    }
+    defender(){
+        this.#bloqueio = true;
     }
 }
