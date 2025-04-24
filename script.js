ace4f5b94cdcd43972d3345c3a916d9df42243c9
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-04-24 07:58:59 -0300

    adicionado métodos para cada uma das classes

--- script.js
+++ script.js
@@ -2,25 +2,41 @@ class Personagem {
     #vida;
     #força;
     #mana;
+    #escudo;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
         this.#vida = 0;
         this.#força = 0;
         this.#mana = 0
     }
+    defender() {
+        this.#escudo = true; 
+    }
 }
 class Guerreiro extends Personagem {
+    #espada;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
+    }
+    atacar(){
+        this.#espada = true;
     }
 }
 class Mago extends Personagem {
+    #magia;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
     }
+    UsarMagia(){
+        this.#magia = true;
+    }
 }
 class Arqueiro extends Personagem {
+    #flecha;
     constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
+    }
+    AtirarFlecha(){
+        this.#flecha = true;
     }
 }
